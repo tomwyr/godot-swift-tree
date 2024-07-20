385d08893e309c946fe115907195d42c64d5b2ed
tomwyr <tomwyr@hotmail.com>
2024-07-20 23:13:38 +0200

    Add generator tests

--- Tests/GodotSwiftTreeTests/GodotSwiftTreeTests.swift
+++ Tests/GodotSwiftTreeTests/GodotSwiftTreeTests.swift
@@ -1,12 +1,40 @@
-@testable import GodotSwiftTree
+import Foundation
 import XCTest
 
 final class GodotSwiftTreeTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
+    func testSingleSceneWithNestedNodes() throws {
+        try test(testCase: "physics-test")
+    }
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    func testSpacesInNodeNames() throws {
+        try test(testCase: "simple")
+    }
+
+    func testMultipleIndependentScenes() throws {
+        try test(testCase: "scene-changer")
+    }
+
+    func testSceneWithNestedScenes() throws {
+        try test(testCase: "waypoints")
+    }
+
+    func testMultipleScenes() throws {
+        try test(testCase: "dodge-the-creeps")
+    }
+    
+    private func test(testCase: String) throws {
+            Log.logger = Logger.stdOut()
+            let project = setUpTestProject(testCase: "simple")
+            _ = try NodeTreeGenerator().generate(project: project)
+    }
+
+    private func setUpTestProject(testCase: String) -> GodotSwiftProject {
+        let testDir = URL(filePath: #file).deletingLastPathComponent()
+        let basePath = testDir.appending(path: "Resources").path()
+
+        return GodotSwiftProject(
+            projectPath: "\(basePath)/\(testCase)/scenes",
+            outputPath: "\(basePath)/\(testCase)/Actual"
+        )
     }
 }
