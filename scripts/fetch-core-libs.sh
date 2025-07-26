c5f997bc81762e61dc48d0b2f5f55a0e5c86440e
Tomasz Wyrowiński <tomwyr@hotmail.com>
2025-07-26 19:38:45 +0200

    Fix workflows issues

--- scripts/fetch-core-libs.sh
+++ scripts/fetch-core-libs.sh
@@ -2,17 +2,21 @@
 
 set -e
 
+# Convert space-separated strings into arrays
+read -ra TARGET_PLATFORMS_ARR <<< "$TARGET_PLATFORMS"
+read -ra LIB_EXTENSIONS_ARR <<< "$LIB_EXTENSIONS"
+
 mkdir -p core-libs
 
-for i in ${!TARGET_PLATFORMS[@]}; do
-  platform=${TARGET_PLATFORMS[$i]}
-  lib_ext=${LIB_EXTENSIONS[$i]}
+for i in ${!TARGET_PLATFORMS_ARR[@]}; do
+  platform=${TARGET_PLATFORMS_ARR[$i]}
+  lib_ext=${LIB_EXTENSIONS_ARR[$i]}
   file_name="libGodotNodeTreeCore-$CORE_LIB_VERSION-$platform$lib_ext"
   url="https://github.com/$CORE_LIB_REPO/releases/latest/download/$file_name"
 
   echo "Downloading $file_name from $url"
   dest_path="core-libs/libGodotNodeTreeCore$lib_ext"
-  curl -f -o "$dest_path" "$url"
+  curl -f -s -o "$dest_path" "$url"
   
   if [[ ! -f "$dest_path" ]]; then
     echo "Failed to download $dest_path" >&2
