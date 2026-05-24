90cba3a2044dd4e90b938eed7ec6d9f3bae4dd48
Tomasz Wyrowiński <tomwyr@hotmail.com>
2026-05-24 23:46:03 +0200

    Make project compatible with Godot 4.6

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 1.2.0 - 2026-05-24
+
+- Made project compatible with Godot 4.6
+
 # 1.1.1 - 2026-05-24
 
 - Fixed validating project path
--- Example/swift/Package.swift
+++ Example/swift/Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/migueldeicaza/SwiftGodot", branch: "main"),
-        .package(url: "https://github.com/tomwyr/godot-swift-tree", from: "1.1.1"),
+        .package(url: "https://github.com/tomwyr/godot-swift-tree", from: "1.2.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
